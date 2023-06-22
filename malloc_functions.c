75633167816391e5d72a9fd157ac6e929749d69a
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-06-23 00:27:32 +0300

    Created malloc_functions.c

---
+++ malloc_functions.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+/**
+ * _calloc - concatenate strings.
+ * @nmemb: elements length.
+ * @size: elements size.
+ * Return: no value.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p = NULL;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)(p) + i) = 0;
+	return (p);
+}
+/**
+ * _realloc - change the size and copy the content.
+ * @ptr: reallocate pointer.
+ * @old_size: old bytes length.
+ * @new_size: new bytes length.
+ * Return: no value.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p = NULL;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			p[i] = *((char *)ptr + i);
+		free(ptr);
+	}
+	else
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		for (i = 0; i < new_size; i++)
+			p[i] = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
+}
